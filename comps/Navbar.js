7b867701ad8df00d58fbd537fdfc7f40e1f8533d
CS <65098478+javascriptseo@users.noreply.github.com>
2022-03-04 20:04:53 -0800

    content

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       </div>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
-      <Link href="/ninjas/"><a>Other Page</a></Link>
+      <Link href="/ninjas/"><a>Other Pages</a></Link>
     </nav>
   );
 }
